2a7db8fae69cea42722c0942687186d3a44c54cd
george <george@parity.io>
2023-09-21 15:23:24 +0100

    amend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import { useDebounce } from "./useDebounce";
 // import { useFetch } from "./useFetch";
 // import useLocalStorage from "./useLocalStorage";
-import useMediaQuery from './useMediaQuery';
+import useMediaQuery from './useMediaQuery.js';
 
 export { useMediaQuery };
